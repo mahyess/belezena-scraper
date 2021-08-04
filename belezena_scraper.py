df81854135e6a4648fcda36af385375acd5da40d
Mahesh <mahyess@users.noreply.github.com>
2021-08-05 02:45:00 +0545

    final

--- belezena_scraper.py
+++ belezena_scraper.py
@@ -2,65 +2,89 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-url = 'https://www.belezanaweb.com.br/institucional/marcas/'
+class Scraper:
+    def __init__(self, get_count):
+        self.count = get_count
+        pass
 
-s = requests.Session()
-headers = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'en-US,en;q=0.9',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4491.0 Safari/537.36'
-}
+    # def count(self):
+    #     return self.count
+    
+    def main(self):
+        url = 'https://belezanaweb.com.br/institucional/marcas/'
 
-def fetch(url, data=None):
-    if data is None:
-        return s.get(url, headers=headers).content
-    else:
-        return s.post(url, data=data).content
+        s = requests.Session()
+        headers = {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US;en;q=0.5',
+            'Connection': 'keep-alive',
+            'DNT':'1',
+            'Host': 'www.belezanaweb.com.br',
+            'Sec-Fetch-Dest':'document',
+            'Sec-Fetch-Mode':'navigate',
+            'Sec-Fetch-Site':'none',
+            'Sec-Fetch-User':'?1',
+            'Sec-GPC':'1',
+            'TE':'trailers',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36',
+        }
 
-content = fetch(url)
-soup = BeautifulSoup(content, 'lxml')
-
-categories = soup.find_all("li", {"class": "sub-menu-item"})
-
-with open('result.csv', mode='w') as csv_file:
-    fieldnames = ['link', 'title', 'photo', 'brand', 'price']
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
-    writer.writeheader()
-
-    for category in categories:
-        if category.parent.get('class', None):
-            # this just starting characters, can be ignored.
-            pass
-        page = 1
-
-        while page:
-            url = f"https://www.belezanaweb.com.br/api/htmls/showcase?uri={category.find('a')['href']}&tab=produtos&size=36&pagina={page}"
-            category_soup = BeautifulSoup(fetch(url), 'lxml')
-
-            items = category_soup.find_all("div", {"class": "showcase-item"})
-
-            for item in items:
-                details = {
-                    "link": "",
-                    "title": "",
-                    "photo": "",
-                    "brand": "",
-                    "price": ""
-                }
-
-                details["link"] = item.find("a", {"class": "showcase-item-title"})["href"]
-                details["title"] = item.find("a", {"class": "showcase-item-title"}).text.strip()
-                details["photo"] = item.find("img")["data-src"]
-                details["brand"] = item.find("span", {"class": "showcase-item-brand"}).text.strip()
-                details["price"] = item.find("span", {"class": "price-value"}).text.strip()
-
-                print(details["title"])
-
-                writer.writerow(details)
-            
-            if len(items):
-                page += 1
+        def fetch(url, data=None):
+            if data is None:
+                return s.get(url, headers=headers).content
             else:
-                page = 0
+                return s.post(url, data=data).content
+
+        content = fetch(url)
+        soup = BeautifulSoup(content, 'lxml')
+
+        categories = soup.find_all("li", {"class": "sub-menu-item"})
+
+        with open('result.csv', mode='w') as csv_file:
+            fieldnames = ['link', 'title', 'photo', 'brand', 'price']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+            writer.writeheader()
+            i = 0
+
+            for category in categories:
+                if category.parent.get('class', None):
+                    # this just starting characters, can be ignored.
+                    pass
+                page = 1
+
+                while page:
+                    url = f"https://belezanaweb.com.br/api/htmls/showcase?uri={category.find('a')['href']}&tab=produtos&size=36&pagina={page}"
+                    category_soup = BeautifulSoup(fetch(url), 'lxml')
+
+                    items = category_soup.find_all("div", {"class": "showcase-item"})
+                    
+                    for item in items:
+                        self.count(i)
+                        i += 1
+                        details = {
+                            "link": "",
+                            "title": "",
+                            "photo": "",
+                            "brand": "",
+                            "price": ""
+                        }
+
+                        details["link"] = item.find("a", {"class": "showcase-item-title"})["href"]
+                        details["title"] = item.find("a", {"class": "showcase-item-title"}).text.strip()
+                        details["photo"] = item.find("img")["data-src"]
+                        details["brand"] = item.find("span", {"class": "showcase-item-brand"}).text.strip()
+                        details["price"] = item.find("span", {"class": "price-value"}).text.strip()
+
+                        print(details["title"])
+
+                        writer.writerow(details)
+                    
+                    if len(items):
+                        page += 1
+                        # i += len(items)
+                    else:
+                        page = 0
+
---
+++ main_window.py
@@ -0,0 +1,44 @@
+from belezena_scraper import Scraper
+from tkinter import Tk, Label, Button, ttk
+    
+
+class MyFirstGUI:
+    def __init__(self, master):
+        self.count = 0
+        self.scrape = Scraper(self.get_count)
+
+        self.master = master
+        master.title("Beleza na bot")
+
+        self.label = Label(master, text="Click download button to run the bot!")
+        self.label.pack()
+
+        self.download_button = Button(master, text="download", command=self.download)
+        self.download_button.pack()
+
+        self.count_label = Label(master, text="Downloaded data: 0")
+        self.count_label.pack()
+
+        self.pb = ttk.Progressbar(
+            master,
+            orient='horizontal',
+            mode='indeterminate',
+            length=280
+        )
+    
+    def get_count(self, count):
+        self.count_label['text'] = f"Downloaded data: {str(count)}"
+        self.master.update()
+
+    def download(self):
+        print("downloading!")
+        self.pb.pack()
+        self.pb.start()
+        self.scrape.main()
+        self.pb.stop()
+        
+
+root = Tk()
+root.geometry("500x500")
+my_gui = MyFirstGUI(root)
+root.mainloop()
