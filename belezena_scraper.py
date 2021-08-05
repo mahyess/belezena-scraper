b170721ee9c39942c9e447fe20ce736cd9b7b4ba
Mahesh <mahyess@users.noreply.github.com>
2021-08-05 16:48:03 +0545

    final final final

--- belezena_scraper.py
+++ belezena_scraper.py
@@ -2,89 +2,110 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
+
 class Scraper:
     def __init__(self, get_count):
         self.count = get_count
         pass
-    
-    def main(self):
-        url = 'https://belezanaweb.com.br/institucional/marcas/'
 
-        s = requests.Session()
+    def fetch(self, url, data=None):
         headers = {
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Accept-Language': 'en-US;en;q=0.5',
-            'Connection': 'keep-alive',
-            'DNT':'1',
-            'Host': 'www.belezanaweb.com.br',
-            'Sec-Fetch-Dest':'document',
-            'Sec-Fetch-Mode':'navigate',
-            'Sec-Fetch-Site':'none',
-            'Sec-Fetch-User':'?1',
-            'Sec-GPC':'1',
-            'TE':'trailers',
-            'Upgrade-Insecure-Requests': '1',
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36',
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "en-US;en;q=0.5",
+            "Connection": "keep-alive",
+            "DNT": "1",
+            "Host": "www.belezanaweb.com.br",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "none",
+            "Sec-Fetch-User": "?1",
+            "Sec-GPC": "1",
+            "TE": "trailers",
+            "Upgrade-Insecure-Requests": "1",
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36",
         }
-
-        def fetch(url, data=None):
+        with requests.Session() as s:
             if data is None:
                 return s.get(url, headers=headers).content
             else:
                 return s.post(url, data=data).content
 
-        content = fetch(url)
-        soup = BeautifulSoup(content, 'lxml')
+    def get_categories(self):
+        url = "https://belezanaweb.com.br/institucional/marcas/"
+
+        content = self.fetch(url)
+        soup = BeautifulSoup(content, "lxml")
 
         categories = soup.find_all("li", {"class": "sub-menu-item"})
 
-        with open('result.csv', mode='w') as csv_file:
-            fieldnames = ['link', 'title', 'photo', 'brand', 'price']
+        for i, category in enumerate(categories):
+            if category.parent.get("class", None):
+                # this just starting characters, can be ignored.
+                del categories[i]
+
+        return [
+            {
+                "title": category.find("a").text.strip(),
+                "url": f"https://belezanaweb.com.br/api/htmls/showcase?uri={category.find('a')['href']}&tab=produtos&size=36",
+            }
+            for category in categories
+        ]
+
+    def get_items(self, category):
+        with open(f'results/{category["title"]}.csv', mode="w") as csv_file:
+            fieldnames = ["link", "title", "photo", "brand", "price"]
             writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
             writer.writeheader()
             i = 0
 
-            for category in categories:
-                if category.parent.get('class', None):
-                    # this just starting characters, can be ignored.
-                    pass
-                page = 1
+            page = 1
 
-                while page:
-                    url = f"https://belezanaweb.com.br/api/htmls/showcase?uri={category.find('a')['href']}&tab=produtos&size=36&pagina={page}"
-                    category_soup = BeautifulSoup(fetch(url), 'lxml')
+            while page:
+                url = f"{category['url']}&pagina={page}"
+                soup = BeautifulSoup(self.fetch(url), "lxml")
 
-                    items = category_soup.find_all("div", {"class": "showcase-item"})
-                    
-                    for item in items:
-                        self.count(i)
-                        i += 1
-                        details = {
-                            "link": "",
-                            "title": "",
-                            "photo": "",
-                            "brand": "",
-                            "price": ""
-                        }
+                items = soup.find_all("div", {"class": "showcase-item"})
 
-                        details["link"] = item.find("a", {"class": "showcase-item-title"})["href"]
-                        details["title"] = item.find("a", {"class": "showcase-item-title"}).text.strip()
-                        details["photo"] = item.find("img")["data-src"]
-                        details["brand"] = item.find("span", {"class": "showcase-item-brand"}).text.strip()
-                        details["price"] = item.find("span", {"class": "price-value"}).text.strip()
+                for item in items:
+                    self.count(i)
+                    i += 1
+                    details = {
+                        "link": "",
+                        "title": "",
+                        "photo": "",
+                        "brand": "",
+                        "price": "",
+                    }
 
-                        print(details["title"])
+                    details["link"] = item.find("a", {"class": "showcase-item-title"})[
+                        "href"
+                    ]
+                    details["title"] = item.find(
+                        "a", {"class": "showcase-item-title"}
+                    ).text.strip()
+                    details["photo"] = item.find("img")["data-src"]
+                    details["brand"] = item.find(
+                        "span", {"class": "showcase-item-brand"}
+                    ).text.strip()
+                    details["price"] = item.find(
+                        "span", {"class": "price-value"}
+                    ).text.strip()
 
-                        details["title"] = details["title"].encode("utf-8")
-                        details['photo'] = details['photo'].replace("w_210", "w_800").replace("h_210", "h_800")
+                    print(details["title"])
 
-                        writer.writerow(details)
-                    
-                    if len(items):
-                        page += 1
-                        # i += len(items)
-                    else:
-                        page = 0
+                    details["title"] = details["title"].encode("utf-8")
+                    details["photo"] = (
+                        details["photo"]
+                        .replace("w_210", "w_800")
+                        .replace("h_210", "h_800")
+                    )
 
+                    writer.writerow(details)
+
+                if len(items):
+                    page += 1
+                    # i += len(items)
+                else:
+                    page = 0
--- main_window.py
+++ main_window.py
@@ -1,44 +1,86 @@
+from tkinter.ttk import Progressbar
 from belezena_scraper import Scraper
-from tkinter import Tk, Label, Button, ttk
-    
+from tkinter import *
 
-class MyFirstGUI:
-    def __init__(self, master):
-        self.count = 0
+# First create application class
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
         self.scrape = Scraper(self.get_count)
+        self.categories = self.scrape.get_categories()
 
-        self.master = master
-        master.title("Beleza na bot")
+        self.pack()
+        self.create_widgets()
 
-        self.label = Label(master, text="Click download button to run the bot!")
-        self.label.pack()
+    def CurSelet(self, evt):
+        self.selected = self.lbox.curselection()[0]
 
-        self.download_button = Button(master, text="download", command=self.download)
-        self.download_button.pack()
-
-        self.count_label = Label(master, text="Downloaded data: 0")
-        self.count_label.pack()
-
-        self.pb = ttk.Progressbar(
-            master,
-            orient='horizontal',
-            mode='indeterminate',
-            length=280
+    def select(self):
+        selected_category = next(
+            item
+            for item in self.categories
+            if item["title"] == self.lbox.get(first=self.selected)
         )
-    
+        if selected_category:
+            self.count_label = Label(self, text="Downloaded data: 0")
+            self.count_label.grid(row=4, column=0, padx=10, pady=3)
+            self.pb = Progressbar(
+                self, orient="horizontal", mode="indeterminate", length=280
+            )
+            self.pb.grid(row=5, column=0, padx=10, pady=3)
+            self.pb.start()
+            self.scrape.get_items(selected_category)
+            self.pb.stop()
+            self.pb.destroy()
+
+    # Create main GUI window
+    def create_widgets(self):
+        self.search_var = StringVar()
+        self.search_var.trace("w", lambda name, index, mode: self.update_list())
+        self.entry = Entry(self, textvariable=self.search_var, width=13)
+        self.lbox = Listbox(self, selectmode=SINGLE, width=45, height=15)
+        self.lbox.bind("<<ListboxSelect>>", self.CurSelet)
+
+        self.entry.grid(row=0, column=0, padx=10, pady=3)
+        self.lbox.grid(row=1, column=0, padx=10, pady=3)
+
+        self.btn = Button(self, text="Download!!", command=self.select, width=20)
+        self.btn.grid(row=2, column=0, padx=10, pady=3)
+
+        self.cursel = StringVar()
+        self.lb1 = Label(self, textvariable=self.cursel)
+        self.lb1.grid(row=3, column=0, padx=10, pady=3)
+
+        # Function for updating the list/doing the search.
+        # It needs to be called here to populate the listbox.
+        self.update_list()
+
+    def update_list(self):
+        search_term = self.search_var.get()
+
+        self.lbox.delete(0, END)
+
+        for item in self.categories:
+            if search_term.lower() in item["title"].lower():
+                self.lbox.insert(END, item["title"])
+
+        allitems = list()
+        for i in range(self.lbox.size()):
+            allitems.append(self.lbox.get(i))
+
     def get_count(self, count):
-        self.count_label['text'] = f"Downloaded data: {str(count)}"
+        self.count_label["text"] = f"Downloaded data: {str(count)}"
         self.master.update()
 
-    def download(self):
-        print("downloading!")
-        self.pb.pack()
-        self.pb.start()
-        self.scrape.main()
-        self.pb.stop()
-        
 
+import os
+
+if not os.path.exists("results"):
+    os.makedirs("results")
+    
 root = Tk()
-root.geometry("300x150")
-my_gui = MyFirstGUI(root)
-root.mainloop()
+root.title("Beleza na bot")
+Label(root, text="Enter text to Search brands/categories.").pack()
+app = Application(master=root)
+print("Starting mainloop()")
+app.mainloop()
